39d9c55b5e8092fbf6f001b7eeb6512b133eaaa8
vlalx <143875984+vlalx@users.noreply.github.com>
2025-06-01 18:56:51 +0300

    Updates in src/components

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,43 +1,45 @@
-import { useLocale } from 'next-intl';
+import { useLocale, useTranslations } from "next-intl";
 import LocaleSwitcherSelect from './LocaleSwitcherSelect';
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
+
+  const t = useTranslations();
 
   return (
     <LocaleSwitcherSelect
       defaultValue={locale}
       items={[
         {
-          value: 'en-US',
-          label: 'Englisch'
+            value: 'en-US',
+            label: 'English'
         },
         {
-          value: 'fr-FR',
-          label: 'French'
+            value: 'fr-FR',
+            label: "Français"
         },
         {
-          value: 'de-DE',
-          label: 'German'
+            value: 'de-DE',
+            label: 'Duits'
         },
         {
-          value: 'it-IT',
-          label: 'Italian'
+            value: 'it-IT',
+            label: 'Italiano'
         },
         {
-          value: 'pl-PL',
-          label: 'Polish'
+            value: 'pl-PL',
+            label: 'Polski'
         },
         {
-          value: 'pt-PT',
-          label: 'Portuguese'
+            value: 'pt-PT',
+            label: 'Português'
         },
         {
-          value: 'tr-TR',
-          label: 'Turkish'
+            value: 'tr-TR',
+            label: 'Türkçe'
         }
       ]}
-      label='Language'
+      label={t('language')}
     />
   );
 }
